56bd30641d05c9e8a8b025f80281c9011a1a9240
Anu <raj.yadav82@gmail.com>
2017-04-21 03:48:27 +1200

    This is my first commit

---
+++ CRUDKnockoutApp/Scripts/ViewModel/TicketViewModel.js
@@ -0,0 +1,12 @@
+﻿
+function TicketsViewModel() {
+    this.tickets = [
+        { name: "Economy", price: 10 },
+        { name: "Business", price: 20 },
+        { name: "FirstClass", price: 50 }
+    ];
+    this.chosenTicket = ko.observable();
+    this.resetTicket=function(){this.chosenTicket(null)}
+
+}
+ko.applyBindings(new TicketsViewModel());
